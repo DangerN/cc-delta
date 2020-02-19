bff06f1c74a3cc5631455b79f35874557573e6d2
Crystal Danger <cdneal091@gmail.com>
2020-02-19 11:10:12 -0500

    changes

--- src/delta/load_image.cr
+++ src/delta/load_image.cr
@@ -1,6 +1,6 @@
 class Delta
   def self.load_image
     test_board
-    board_by_id("test").add_thread(%({"id":0,"flags":["sfw"],"posts":[]}))
+    board_by_id("test").add_thread(%({"id":0,"flags":["sfw"],"posts":[{"id":0,"badges":[],"flags":[],"name":"yingus","text":"I am lonely post!","media_name":"cute-potato"}]}))
   end
 end
