b43cc26d65075943706756f701866ab580ccf12d
Crystal Danger <cdneal091@gmail.com>
2020-02-22 15:14:02 -0500

    progress

--- src/delta.cr
+++ src/delta.cr
@@ -3,17 +3,25 @@ require "redis"
 require "cc-alpha"
 require "./delta/*"
 
+
+# post_json = redis.lindex("board:test:thread:1:posts", "0")
+#
+# p post_json
+# if (!post_json.nil?)
+#   post = Alpha::Post.from_json(post_json)
+# end
+#
+# p post
+
 class Delta
   VERSION = "0.1.0"
   @@updateTargets = [] of HTTP::WebSocket
   @@redis = Redis.new
 
-  log_message(@@redis.get("test"))
-
   def self.run
-    kemal_config
+    # kemal_config
     load_image
-    Kemal.run
+    # Kemal.run
   end
 end
 
@@ -29,6 +37,12 @@ module Alpha
       @flags = [] of String
     end
   end
+  class Board
+    def initialize(id, name, @flags = [] of String, @thread_limit = 25.to_u8 , @threads = {} of String => Thread , @post_count = 0.to_u64, @thread_count = 0.to_u64)
+      @id = id
+      @name = name
+    end
+  end
 end
 
-# Delta.run
+Delta.run
--- src/delta/load_image.cr
+++ src/delta/load_image.cr
@@ -1,6 +1,21 @@
 class Delta
   def self.load_image
-    test_board
-    board_by_id("test").add_thread(%({"id":0,"flags":["sfw"],"posts":[{"id":0,"badges":[],"flags":[],"name":"yingus","text":"I am lonely post!","media_name":"cute-potato"}]}))
+    # test_board
+    # board_by_id("test").add_thread(%({"id":0,"flags":["sfw"],"posts":[{"id":0,"badges":[],"flags":[],"name":"yingus","text":"I am lonely post!","media_name":"cute-potato"}]}))
+    board_list = @@redis.lrange("board_list", "0", "-1")
+    board_list.not_nil!.each do |board_id|
+      board_meta = @@redis.hgetall("board:#{board_id}").not_nil!
+      board_hash = {} of String => String
+      while board_meta.size > 1
+        board_hash[board_meta.shift.to_s] = board_meta.shift.to_s
+      end
+      p board_hash
+      board = Alpha::Board.new(board_hash["id"],board_hash["name"])
+      p board
+      @@redis.keys("board:#{board_id}:thread:*").not_nil!.each do |thread|
+        p @@redis.lrange(thread.to_s, "0", "-1")
+      end
+
+    end
   end
 end
