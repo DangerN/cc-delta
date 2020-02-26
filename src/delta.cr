fd89489b9eb2fba378421e10e350044f50379f01
Crystal Danger <cdneal091@gmail.com>
2020-02-26 15:09:26 -0500

    removed redis and added postgres

--- src/delta.cr
+++ src/delta.cr
@@ -6,10 +6,8 @@ require "./delta/*"
 class Delta
   VERSION = "0.1.0"
   @@updateTargets = [] of HTTP::WebSocket
-  @@redis = Redis.new
 
-  log_message(@@redis.get("test"))
-
+  
   def self.run
     kemal_config
     load_image
