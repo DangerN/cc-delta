9273f24e323f76b923a667e3be81bf7965cc6ed2
Crystal Danger <cdneal091@gmail.com>
2020-03-25 14:06:58 -0400

    moved form data processing to handler and cleaned up code

--- src/delta/handlers.cr
+++ src/delta/handlers.cr
@@ -1,18 +1,45 @@
 class Delta
-  def self.log_message(message)
-    p message
+  def self.process_formData(request)
+    post_params = {} of String => String
+
+    HTTP::FormData.parse(request) do |part|
+      case part.name
+      when "name"
+        post_params.["name"] = part.body.gets_to_end
+      when "text"
+        post_params.["text"] = part.body.gets_to_end
+      when "subject"
+        post_params.["subject"] = part.body.gets_to_end
+      when "thread"
+        post_params.["thread"] = part.body.gets_to_end
+      when "board"
+        post_params.["board"] = part.body.gets_to_end
+      when "file"
+        if (!part.filename.nil?)
+          file_parts = part.filename.not_nil!.split('.')
+          echo_connection = HTTP::Client.new(host: "0.0.0.0", port: "3001")
+          headers = HTTP::Headers{
+            "Content-Type"   => "application/octet-stream",
+            "File-Name"      => file_parts[0],
+            "File-Extension" => file_parts[1],
+          }
+          post_params.["media"] = file_parts[0]
+          # TODO: This needs to verify successful upload and handle failure
+          response = echo_connection.post("/media", headers: headers, body: part.body.gets_to_end)
+        end
+      end
+    end
+    post_params
   end
 
   def self.handle_new_post(params)
     db = DB.open "postgres://localhost:5432/cc-db"
     board_id = params["board_id"]
     thread = params["thread"]
-    p "attempt to insert into database"
     db.query("insert into \"#{params["board_id"]}_posts\" (badges, flags, media_name, subject, name, text, thread_id) values ($1, $2, $3, $4, $5, $6, $7) returning *;", params["badges"], params["flags"], params["media"], params["subject"], params["name"], params["text"], params["thread"]) do |rs|
       rs.each do
         id, badges, flags, media_name, subject, name, text, time_stamp =
           rs.read(Int64, Array(String), Array(String), String, String, String, String, Time)
-        p "attempt to add post"
         Alpha.boards[board_id].threads[thread.to_s].posts.push(Alpha::Post.new(id.to_u64, name, subject, text, media_name, badges, flags, time_stamp))
       end
     end
@@ -25,13 +52,10 @@ class Delta
     thread = nil
     db = DB.open "postgres://localhost:5432/cc-db"
     db.query("insert into \"#{board_id}_threads\" (flags) values ($1) returning *", flags) do |rs|
-      p "added thread"
       rs.each do
         id, flags, post_limit = rs.read(Int32, Array(String), Int16)
         thread = id.not_nil!
-        p "inserting thread"
         Alpha.boards[board_id].threads[thread.to_s] = Alpha::Thread.new(id.to_u64, flags, post_limit.to_u16)
-        p "inserted thread"
       end
     end
 
@@ -45,6 +69,5 @@ class Delta
     end
 
     db.close
-    p thread
   end
 end
--- src/delta/kemal_config.cr
+++ src/delta/kemal_config.cr
@@ -1,11 +1,11 @@
 class Delta
-
   # Configure the behavior of the server
   private def self.kemal_config
     ws "/" do |socket|
       @@updateTargets.push socket
       puts "Socket opened: #{socket}"
       socket.send Alpha.boards.to_json
+
       # Remove target from list on socket close.
       socket.on_close do |_|
         @@updateTargets.delete socket
@@ -15,62 +15,20 @@ class Delta
 
     options "/new" do |env|
       headers(env, {
-        "Access-Control-Allow-Origin" => "*",
-        "Access-Control-Allow-Headers" => "content-type"
-        })
+        "Access-Control-Allow-Origin"  => "*",
+        "Access-Control-Allow-Headers" => "content-type",
+      })
     end
 
-    #
     post "/new" do |env|
-      # name = ""
-      # text = ""
-      # subject = ""
-      # thread = nil
-      # board_id = nil
-      # media = ""
-      # badges = [] of String
-      # flags = [] of String
-
-      post_params = {} of String => String
-
       headers(env, {"Access-Control-Allow-Origin" => "*"})
-      HTTP::FormData.parse(env.request) do |part|
-        case part.name
-        when "name"
-          name = part.body.gets_to_end
-          post_params.["name"] = part.body.gets_to_end
-        when "text"
-          text = part.body.gets_to_end
-          post_params.["text"] = part.body.gets_to_end
-        when "subject"
-          subject = part.body.gets_to_end
-          post_params.["subject"] = part.body.gets_to_end
-        when "thread"
-          thread = part.body.gets_to_end
-          post_params.["thread"] = part.body.gets_to_end
-        when "board"
-          board_id = part.body.gets_to_end
-          post_params.["board"] = part.body.gets_to_end
-        when "file"
-          if (!part.filename.nil?)
-            file_parts = part.filename.not_nil!.split('.')
-            echo_connection = HTTP::Client.new(host: "0.0.0.0", port: "3001")
-            headers = HTTP::Headers{
-              "Content-Type"=>"application/octet-stream",
-              "File-Name"=> file_parts[0],
-              "File-Extension"=> file_parts[1]
-            }
-            media = file_parts[0]
-            post_params.["media"] = file_parts[0]
-            response = echo_connection.post("/media", headers: headers, body: part.body.gets_to_end)
-          end
-        end
-      end
 
-      if (post_params["thread"] != nil)
-        handle_new_post(post_params)
+      form_data = process_formData(env.request)
+
+      if (form_data["thread"] != nil)
+        handle_new_post(form_data)
       else
-        handle_new_thread(post_params, [""])
+        handle_new_thread(form_data, [""])
       end
 
       @@updateTargets.each { |socket| socket.send Alpha.boards.to_json }
